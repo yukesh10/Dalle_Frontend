439c64b27e28477e180da32862c79d7b0bc2b1e3
Yukesh Sitoula <ys2ck@virginia.edu>
2023-09-25 20:28:11 -0400

    Testing CI/CD

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -63,7 +63,7 @@ const Home = () => {
     <section className='max-w-7xl mx-auto'>
         <div>
             <h1 className='font-extrabold text-[#222328] text-[32px]'>
-                The Community Showcase
+                The Community Showcase -- Testing
             </h1>
             <p className='mt-2 text-[#666e75] text-[14px] max-w-[500px]'>
                 Browse through a collection of imaginative and visually stunning images generatred by DALL-E AI
